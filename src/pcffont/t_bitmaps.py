b6e4e021fede66bef0022354d0ffd69684ac93cc
TakWolf <takwolf@foxmail.com>
2024-04-11 13:43:40 +0800

    完成 bitmaps 表

--- src/pcffont/t_bitmaps.py
+++ src/pcffont/t_bitmaps.py
@@ -1,29 +1,115 @@
+from collections import UserList
+
 from pcffont.header import PcfTableFormat, PcfHeader
 from pcffont.internal import util
 from pcffont.internal.buffer import Buffer
 from pcffont.table import PcfTable
 
 
-# TODO
-class PcfBitmaps(PcfTable):
+def _get_bit_aligned_bitmap(bitmap: list[list[int]], n: int) -> list[list[int]]:
+    new_bitmap = []
+    for bitmap_row in bitmap:
+        new_bitmap_row = []
+        for i in range(n):
+            if i < len(bitmap_row):
+                new_bitmap_row.append(bitmap_row[i])
+            else:
+                new_bitmap_row.append(0)
+        new_bitmap.append(new_bitmap_row)
+    return new_bitmap
+
+
+class PcfBitmaps(PcfTable, UserList[list[list[int]]]):
     @staticmethod
     def parse(buffer: Buffer, header: PcfHeader) -> 'PcfBitmaps':
         table_format = util.read_and_check_table_format(buffer, header)
         byte_order = util.get_table_byte_order(table_format)
 
-        # TODO
-        obj = PcfBitmaps(table_format)
-        buffer.seek(header.table_offset)
-        obj.chunk = buffer.read(header.table_size)
-        return obj
+        # How each row in each glyph's bitmap is padded
+        # 0 => byte, 1 => short, 2 => int32, 3 => int64
+        bitmap_padded_mode = table_format & 3
+        bitmap_row_size = [1, 2, 4, 8][bitmap_padded_mode]
+
+        # What the bits are stored
+        # 0 => byte, 1 => short, 2 => int32
+        bits_stored_mode = (table_format >> 4) & 3
+        assert bits_stored_mode == 0  # TODO
+
+        glyphs_count = buffer.read_int32(byte_order)
+        bitmap_offsets = [buffer.read_int32(byte_order) for _ in range(glyphs_count)]
+        size_configs = [buffer.read_int32(byte_order) for _ in range(4)]
+        bitmaps_start = buffer.tell()
+        bitmaps_size = size_configs[bitmap_padded_mode]
+
+        bitmaps = []
+        for i in range(glyphs_count):
+            bitmap_offset = bitmap_offsets[i]
+            if i < glyphs_count - 1:
+                bitmap_offset_next = bitmap_offsets[i + 1]
+            else:
+                bitmap_offset_next = bitmaps_size
+            bitmap_size = bitmap_offset_next - bitmap_offset
+
+            bitmap = []
+            buffer.seek(bitmaps_start + bitmap_offset)
+            for _ in range(bitmap_size // bitmap_row_size):
+                bitmap_row_data = buffer.read(bitmap_row_size)
+                bin_string = ''.join([f'{b:08b}' for b in bitmap_row_data])
+                bitmap_row = [int(c) for c in bin_string]
+                bitmap.append(bitmap_row)
+            bitmaps.append(bitmap)
+
+        return PcfBitmaps(table_format, bitmaps)
 
     def __init__(
             self,
             table_format: int = PcfTableFormat.BYTE_ORDER_BIG,
+            bitmaps: list[list[list[int]]] = None,
     ):
         PcfTable.__init__(self, table_format)
+        UserList.__init__(self, bitmaps)
 
     def _dump(self, buffer: Buffer, table_offset: int) -> int:
-        # TODO
+        byte_order = util.get_table_byte_order(self.table_format)
+
+        # How each row in each glyph's bitmap is padded
+        # 0 => byte, 1 => short, 2 => int32, 3 => int64
+        bitmap_padded_mode = self.table_format & 3
+        bitmap_row_size = [1, 2, 4, 8][bitmap_padded_mode]
+
+        # What the bits are stored
+        # 0 => byte, 1 => short, 2 => int32
+        bits_stored_mode = (self.table_format >> 4) & 3
+        assert bits_stored_mode == 0  # TODO
+
+        glyphs_count = len(self)
+
+        bitmaps_start = table_offset + 4 + 4 + 4 * glyphs_count + 4 * 4
+        bitmaps_size = 0
+        bitmap_offsets = []
+        buffer.seek(bitmaps_start)
+        for bitmap in self:
+            bitmap_offsets.append(bitmaps_size)
+            bitmap = _get_bit_aligned_bitmap(bitmap, 8 * bitmap_row_size)
+            for bitmap_row in bitmap:
+                bin_string = ''.join(map(str, bitmap_row))
+                bitmap_row_data = int(bin_string, 2).to_bytes(len(bin_string) // 8, 'big')
+                bitmaps_size += buffer.write(bitmap_row_data)
+
+        size_configs = [
+            bitmaps_size // 4,
+            bitmaps_size // 2,
+            bitmaps_size,
+            bitmaps_size * 2,
+        ]
+
         buffer.seek(table_offset)
-        return buffer.write(self.chunk)
+        buffer.write_int32(glyphs_count, byte_order)
+        for offset in bitmap_offsets:
+            buffer.write_int32(offset, byte_order)
+        for size_config in size_configs:
+            buffer.write_int32(size_config, byte_order)
+        buffer.skip(bitmaps_size)
+
+        table_size = buffer.tell() - table_offset
+        return table_size
