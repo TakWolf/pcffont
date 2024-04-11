2e331c57c5c01129664af57977feaff9c2381a41
TakWolf <takwolf@foxmail.com>
2024-04-11 15:05:35 +0800

    添加 Demo

--- examples/demo.py
+++ examples/demo.py
@@ -1,19 +1,3 @@
-# PcfFont
-
-[![Python](https://img.shields.io/badge/python-3.11-brightgreen)](https://www.python.org)
-[![PyPI](https://img.shields.io/pypi/v/pcffont)](https://pypi.org/project/pcffont/)
-
-PcfFont is a library for manipulating [Portable Compiled Format (PCF) Fonts](https://en.wikipedia.org/wiki/Portable_Compiled_Format).
-
-## Installation
-
-```shell
-pip install pcffont
-```
-
-## Usage
-
-```python
 import os
 import shutil
 
@@ -42,17 +26,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-```
-
-## Test Fonts
-
-- [GNU Unifont Glyphs](https://unifoundry.com/unifont/index.html)
-- [bitmap-fonts](https://github.com/masaeedu/bitmap-fonts)
-
-## References
-
-- [FontForge - The X11 PCF bitmap font file format](https://fontforge.org/docs/techref/pcf-format.html)
-
-## License
-
-Under the [MIT license](LICENSE).
