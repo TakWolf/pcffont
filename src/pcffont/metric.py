ab98f50d80029d07a71738ab0ba1dd86e47692af
TakWolf <takwolf@foxmail.com>
2024-04-12 11:44:05 +0800

    修复 PcfMetric 压缩模式下，值未应用偏移 0x80 导致不正确的问题

--- src/pcffont/metric.py
+++ src/pcffont/metric.py
@@ -5,11 +5,11 @@ class PcfMetric:
     @staticmethod
     def parse(buffer: Buffer, byte_order: ByteOrder, is_compressed: bool) -> 'PcfMetric':
         if is_compressed:
-            left_sided_bearing = buffer.read_int8(byte_order)
-            right_side_bearing = buffer.read_int8(byte_order)
-            character_width = buffer.read_int8(byte_order)
-            character_ascent = buffer.read_int8(byte_order)
-            character_descent = buffer.read_int8(byte_order)
+            left_sided_bearing = buffer.read_int8(byte_order) - 0x80
+            right_side_bearing = buffer.read_int8(byte_order) - 0x80
+            character_width = buffer.read_int8(byte_order) - 0x80
+            character_ascent = buffer.read_int8(byte_order) - 0x80
+            character_descent = buffer.read_int8(byte_order) - 0x80
             character_attributes = 0
         else:
             left_sided_bearing = buffer.read_int16(byte_order)
@@ -45,11 +45,11 @@ class PcfMetric:
 
     def dump(self, buffer: Buffer, byte_order: ByteOrder, is_compressed: bool):
         if is_compressed:
-            buffer.write_int8(self.left_sided_bearing, byte_order)
-            buffer.write_int8(self.right_side_bearing, byte_order)
-            buffer.write_int8(self.character_width, byte_order)
-            buffer.write_int8(self.character_ascent, byte_order)
-            buffer.write_int8(self.character_descent, byte_order)
+            buffer.write_int8(self.left_sided_bearing + 0x80, byte_order)
+            buffer.write_int8(self.right_side_bearing + 0x80, byte_order)
+            buffer.write_int8(self.character_width + 0x80, byte_order)
+            buffer.write_int8(self.character_ascent + 0x80, byte_order)
+            buffer.write_int8(self.character_descent + 0x80, byte_order)
         else:
             buffer.write_int16(self.left_sided_bearing, byte_order)
             buffer.write_int16(self.right_side_bearing, byte_order)
