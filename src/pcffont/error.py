d1ef242dcb182a5f7ff5be425918c9b72be81521
TakWolf <takwolf@foxmail.com>
2024-04-29 14:41:07 +0800

    代码整理

--- src/pcffont/error.py
+++ src/pcffont/error.py
@@ -13,24 +13,24 @@ class PcfTableTypeError(PcfError):
 
 class PcfPropKeyError(PcfError):
     def __init__(self, key: str, reason: str):
+        super().__init__(f"'{key}': {reason}")
         self.key = key
         self.reason = reason
-        super().__init__(f"'{key}': {reason}")
 
 
 class PcfPropValueError(PcfError):
     def __init__(self, key: str, value: object, reason: str):
+        super().__init__(f"'{key}': '{value}': {reason}")
         self.key = key
         self.value = value
         self.reason = reason
-        super().__init__(f"'{key}': '{value}': {reason}")
 
 
 class PcfXlfdError(PcfError):
     def __init__(self, font_name: str, reason: str):
+        super().__init__(f"'{font_name}': {reason}")
         self.font_name = font_name
         self.reason = reason
-        super().__init__(f"'{font_name}': {reason}")
 
 
 class PcfOutOfRangeError(PcfError):
