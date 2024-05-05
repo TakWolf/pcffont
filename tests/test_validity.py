0ef068271615de8497bb2d3ea0a0e988d70b4aa9
TakWolf <takwolf@foxmail.com>
2024-05-06 05:34:29 +0800

    通过 BDF 交叉校验

--- tests/test_validity.py
+++ tests/test_validity.py
@@ -1,11 +1,14 @@
 import os
 
+from bdffont import BdfFont
+
 from pcffont import PcfFont
 
 project_root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 
 def test_demo():
+    font_0 = BdfFont.load(os.path.join(project_root_dir, 'assets', 'demo', 'demo.bdf'))
     font_1 = PcfFont.load(os.path.join(project_root_dir, 'assets', 'demo', 'demo-lsbyte-lsbit-p4-u2.pcf'))
     font_2 = PcfFont.load(os.path.join(project_root_dir, 'assets', 'demo', 'demo-lsbyte-msbit-p4-u2.pcf'))
     font_3 = PcfFont.load(os.path.join(project_root_dir, 'assets', 'demo', 'demo-msbyte-lsbit-p4-u2.pcf'))
@@ -15,8 +18,18 @@ def test_demo():
     font_7 = PcfFont.load(os.path.join(project_root_dir, 'assets', 'demo', 'demo-msbyte-lsbit-p2-u4.pcf'))
     font_8 = PcfFont.load(os.path.join(project_root_dir, 'assets', 'demo', 'demo-msbyte-msbit-p2-u4.pcf'))
 
-    assert len(font_1.metrics) == len(font_2.metrics) == len(font_3.metrics) == len(font_4.metrics) == len(font_5.metrics) == len(font_6.metrics) == len(font_7.metrics) == len(font_8.metrics)
-    for glyph_index, metric_1 in enumerate(font_1.metrics):
+    for glyph_index, glyph in enumerate(font_0.glyphs):
+        glyph_name_1 = font_1.glyph_names[glyph_index]
+        glyph_name_2 = font_2.glyph_names[glyph_index]
+        glyph_name_3 = font_3.glyph_names[glyph_index]
+        glyph_name_4 = font_4.glyph_names[glyph_index]
+        glyph_name_5 = font_5.glyph_names[glyph_index]
+        glyph_name_6 = font_6.glyph_names[glyph_index]
+        glyph_name_7 = font_7.glyph_names[glyph_index]
+        glyph_name_8 = font_8.glyph_names[glyph_index]
+        assert glyph.name == glyph_name_1 == glyph_name_2 == glyph_name_3 == glyph_name_4 == glyph_name_5 == glyph_name_6 == glyph_name_7 == glyph_name_8
+
+        metric_1 = font_1.metrics[glyph_index]
         metric_2 = font_2.metrics[glyph_index]
         metric_3 = font_3.metrics[glyph_index]
         metric_4 = font_4.metrics[glyph_index]
@@ -30,9 +43,11 @@ def test_demo():
         assert metric_1.ascent == metric_2.ascent == metric_3.ascent == metric_4.ascent == metric_5.ascent == metric_6.ascent == metric_7.ascent == metric_8.ascent
         assert metric_1.descent == metric_2.descent == metric_3.descent == metric_4.descent == metric_5.descent == metric_6.descent == metric_7.descent == metric_8.descent
         assert metric_1.attributes == metric_2.attributes == metric_3.attributes == metric_4.attributes == metric_5.attributes == metric_6.attributes == metric_7.attributes == metric_8.attributes
+        assert glyph.device_width_x == metric_1.character_width == metric_2.character_width == metric_3.character_width == metric_4.character_width == metric_5.character_width == metric_6.character_width == metric_7.character_width == metric_8.character_width
+        assert glyph.dimensions == metric_1.dimensions == metric_2.dimensions == metric_3.dimensions == metric_4.dimensions == metric_5.dimensions == metric_6.dimensions == metric_7.dimensions == metric_8.dimensions
+        assert glyph.origin == metric_1.origin == metric_2.origin == metric_3.origin == metric_4.origin == metric_5.origin == metric_6.origin == metric_7.origin == metric_8.origin
 
-    assert len(font_1.bitmaps) == len(font_2.bitmaps) == len(font_3.bitmaps) == len(font_4.bitmaps) == len(font_5.bitmaps) == len(font_6.bitmaps) == len(font_7.bitmaps) == len(font_8.bitmaps)
-    for glyph_index, bitmap_1 in enumerate(font_1.bitmaps):
+        bitmap_1 = font_1.bitmaps[glyph_index]
         bitmap_2 = font_2.bitmaps[glyph_index]
         bitmap_3 = font_3.bitmaps[glyph_index]
         bitmap_4 = font_4.bitmaps[glyph_index]
@@ -40,8 +55,8 @@ def test_demo():
         bitmap_6 = font_6.bitmaps[glyph_index]
         bitmap_7 = font_7.bitmaps[glyph_index]
         bitmap_8 = font_8.bitmaps[glyph_index]
-        assert len(bitmap_1) == len(bitmap_2) == len(bitmap_3) == len(bitmap_4) == len(bitmap_5) == len(bitmap_6) == len(bitmap_7) == len(bitmap_8)
-        for i, bitmap_row_1 in enumerate(bitmap_1):
+        for i, bitmap_row_0 in enumerate(glyph.bitmap):
+            bitmap_row_1 = bitmap_1[i]
             bitmap_row_2 = bitmap_2[i]
             bitmap_row_3 = bitmap_3[i]
             bitmap_row_4 = bitmap_4[i]
@@ -49,7 +64,7 @@ def test_demo():
             bitmap_row_6 = bitmap_6[i]
             bitmap_row_7 = bitmap_7[i]
             bitmap_row_8 = bitmap_8[i]
-            assert len(bitmap_row_1) == len(bitmap_row_2) == len(bitmap_row_3) == len(bitmap_row_4) == len(bitmap_row_5) == len(bitmap_row_6) == len(bitmap_row_7) == len(bitmap_row_8)
+            bin_string_0 = ''.join(map(str, bitmap_row_0))
             bin_string_1 = ''.join(map(str, bitmap_row_1))
             bin_string_2 = ''.join(map(str, bitmap_row_2))
             bin_string_3 = ''.join(map(str, bitmap_row_3))
@@ -58,4 +73,25 @@ def test_demo():
             bin_string_6 = ''.join(map(str, bitmap_row_6))
             bin_string_7 = ''.join(map(str, bitmap_row_7))
             bin_string_8 = ''.join(map(str, bitmap_row_8))
-            assert bin_string_1 == bin_string_2 == bin_string_3 == bin_string_4 == bin_string_5 == bin_string_6 == bin_string_7 == bin_string_8
+            assert bin_string_0 == bin_string_1 == bin_string_2 == bin_string_3 == bin_string_4 == bin_string_5 == bin_string_6 == bin_string_7 == bin_string_8
+
+
+def test_unifont():
+    font_1 = BdfFont.load(os.path.join(project_root_dir, 'assets', 'unifont', 'unifont-15.1.05.bdf'))
+    font_2 = PcfFont.load(os.path.join(project_root_dir, 'assets', 'unifont', 'unifont-15.1.05.pcf'))
+
+    for glyph_index, glyph in enumerate(font_1.glyphs):
+        glyph_name = font_2.glyph_names[glyph_index]
+        assert glyph.name == glyph_name
+
+        metric = font_2.metrics[glyph_index]
+        assert glyph.device_width_x == metric.character_width
+        assert glyph.dimensions == metric.dimensions
+        assert glyph.origin == metric.origin
+
+        bitmap = font_2.bitmaps[glyph_index]
+        for i, bitmap_row_1 in enumerate(glyph.bitmap):
+            bitmap_row_2 = bitmap[i]
+            bin_string_1 = ''.join(map(str, bitmap_row_1))
+            bin_string_2 = ''.join(map(str, bitmap_row_2))
+            assert bin_string_1 == bin_string_2
